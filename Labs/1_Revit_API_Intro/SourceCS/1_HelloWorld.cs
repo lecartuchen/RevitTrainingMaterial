34200fb722f76c2e8cf199e3efb83255a097099f
Lecartuchen <lecartuchen@gmail.com>
2025-01-28 23:55:17 -0500

    Added text to TaskDialog.Show().

--- Labs/1_Revit_API_Intro/SourceCS/1_HelloWorld.cs
+++ Labs/1_Revit_API_Intro/SourceCS/1_HelloWorld.cs
@@ -21,11 +21,10 @@
 #endregion // Copyright
 
 #region Namespaces
-using System;
+using Autodesk.Revit.ApplicationServices; // This is for Revit Application
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Autodesk.Revit.Attributes;
-using Autodesk.Revit.ApplicationServices; // This is for Revit Application
 #endregion
 
 #region Description
@@ -40,145 +39,145 @@ using Autodesk.Revit.ApplicationServices; // This is for Revit Application
 
 namespace IntroCs
 {
-  /// <summary>
-  /// Hello World #1 - A minimum Revit external command
-  /// with all namespaces fully qualified.
-  /// </summary>
-  [Autodesk.Revit.Attributes.Transaction(
-    Autodesk.Revit.Attributes.TransactionMode.Manual )]
-  public class HelloWorld : Autodesk.Revit.UI.IExternalCommand
-  {
-    public Autodesk.Revit.UI.Result Execute(
-      Autodesk.Revit.UI.ExternalCommandData commandData,
-      ref string message,
-      Autodesk.Revit.DB.ElementSet elements )
+    /// <summary>
+    /// Hello World #1 - A minimum Revit external command
+    /// with all namespaces fully qualified.
+    /// </summary>
+    [Autodesk.Revit.Attributes.Transaction(
+    Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class HelloWorld : Autodesk.Revit.UI.IExternalCommand
     {
-      Autodesk.Revit.UI.TaskDialog.Show(
-        "My Dialog Title",
-        "Hello World!" );
+        public Autodesk.Revit.UI.Result Execute(
+          Autodesk.Revit.UI.ExternalCommandData commandData,
+          ref string message,
+          Autodesk.Revit.DB.ElementSet elements)
+        {
+            Autodesk.Revit.UI.TaskDialog.Show(
+              "My Dialog Title",
+              "Hello World! \nTesting the TaskDialog.Show()");
 
-      return Autodesk.Revit.UI.Result.Succeeded;
-    }
-  }
-
-  /// <summary>
-  /// Hello World #2 - simplified without full namespace
-  /// and using ReadOnly attribute.
-  /// </summary>
-  [Transaction( TransactionMode.ReadOnly )]
-  public class HelloWorldSimple : IExternalCommand
-  {
-    public Result Execute(
-      ExternalCommandData commandData,
-      ref string message,
-      ElementSet elements )
-    {
-      TaskDialog.Show(
-        "My Dialog Title",
-        "Hello World Simple!" );
-
-      return Result.Succeeded;
-    }
-  }
-
-  /// <summary>
-  /// Hello World #3 - minimum external application
-  /// Difference: IExternalApplication instead of IExternalCommand.
-  /// In addin manifest, use addin type "Application" and Name instead of Text tag.
-  /// </summary>
-  public class HelloWorldApp : IExternalApplication
-  {
-    // OnStartup() - called when Revit starts.
-
-    public Result OnStartup( UIControlledApplication app )
-    {
-      TaskDialog.Show( "My Dialog Title", "Hello World from App!" );
-
-      return Result.Succeeded;
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
     }
 
-    // OnShutdown() - called when Revit ends.
-
-    public Result OnShutdown( UIControlledApplication app )
+    /// <summary>
+    /// Hello World #2 - simplified without full namespace
+    /// and using ReadOnly attribute.
+    /// </summary>
+    [Transaction(TransactionMode.ReadOnly)]
+    public class HelloWorldSimple : IExternalCommand
     {
-      return Result.Succeeded;
-    }
-  }
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            TaskDialog.Show(
+              "My Dialog Title",
+              "Hello World Simple!");
 
-  /// <summary>
-  /// Command Arguments
-  /// Take a look at the command arguments.
-  /// commandData is the topmost object and
-  /// provides the entry point to the Revit model.
-  /// </summary>
-  [Transaction( TransactionMode.ReadOnly )]
-  public class CommandData : IExternalCommand
-  {
-    public Result Execute(
-      ExternalCommandData commandData,
-      ref string message,
-      ElementSet elements )
-    {
-      // The first argument, commandData, provides access to the top most object model.
-      // You will get the necessary information from commandData.
-      // To see what's in there, print out a few data accessed from commandData
-      //
-      // Exercise: Place a break point at commandData and drill down the data.
-
-      UIApplication uiApp = commandData.Application;
-      Application rvtApp = uiApp.Application;
-      UIDocument uiDoc = uiApp.ActiveUIDocument;
-      Document rvtDoc = uiDoc.Document;
-
-      // Print out a few information that you can get from commandData
-      string versionName = rvtApp.VersionName;
-      string documentTitle = rvtDoc.Title;
-
-      TaskDialog.Show(
-        "Revit Intro Lab",
-        "Version Name = " + versionName
-        + "\nDocument Title = " + documentTitle );
-
-      // Print out a list of wall types available in the current rvt project:
-
-      //WallTypeSet wallTypes = rvtDoc.WallTypes; // 2013, deprecated in 2014
-
-      FilteredElementCollector wallTypes // 2014
-        = new FilteredElementCollector( rvtDoc )
-          .OfClass( typeof( WallType ) );
-
-      string s = "";
-      foreach( WallType wallType in wallTypes )
-      {
-        s += wallType.Name + "\r\n";
-      }
-
-      // Show the result:
-
-      TaskDialog.Show(
-        "Revit Intro Lab",
-        "Wall Types (in main instruction):\n\n" + s );
-
-      // Return failure code to demonstrate output argument handling.
-      // 2nd and 3rd arguments are displayed to the user when the command fails.
-      // 2nd - set a message to display to the user.
-      // 3rd - set elements to highlight on screen.
-
-      return Result.Failed;
+            return Result.Succeeded;
+        }
     }
 
-    #region Revit Macros
-    public void MyFirstMacro()
+    /// <summary>
+    /// Hello World #3 - minimum external application
+    /// Difference: IExternalApplication instead of IExternalCommand.
+    /// In addin manifest, use addin type "Application" and Name instead of Text tag.
+    /// </summary>
+    public class HelloWorldApp : IExternalApplication
     {
-      TaskDialog.Show( "My First Macro", "Hello World!" );
+        // OnStartup() - called when Revit starts.
+
+        public Result OnStartup(UIControlledApplication app)
+        {
+            TaskDialog.Show("My Dialog Title", "Hello World from App!");
+
+            return Result.Succeeded;
+        }
+
+        // OnShutdown() - called when Revit ends.
+
+        public Result OnShutdown(UIControlledApplication app)
+        {
+            return Result.Succeeded;
+        }
     }
 
-    public void MyFirstMacro2()
+    /// <summary>
+    /// Command Arguments
+    /// Take a look at the command arguments.
+    /// commandData is the topmost object and
+    /// provides the entry point to the Revit model.
+    /// </summary>
+    [Transaction(TransactionMode.ReadOnly)]
+    public class CommandData : IExternalCommand
     {
-      TaskDialog.Show( "My First Macro",
-        "The current model file is " );
-        //+ this.Application.ActiveUIDocument.Document.PathName );
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            // The first argument, commandData, provides access to the top most object model.
+            // You will get the necessary information from commandData.
+            // To see what's in there, print out a few data accessed from commandData
+            //
+            // Exercise: Place a break point at commandData and drill down the data.
+
+            UIApplication uiApp = commandData.Application;
+            Application rvtApp = uiApp.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document rvtDoc = uiDoc.Document;
+
+            // Print out a few information that you can get from commandData
+            string versionName = rvtApp.VersionName;
+            string documentTitle = rvtDoc.Title;
+
+            TaskDialog.Show(
+              "Revit Intro Lab",
+              "Version Name = " + versionName
+              + "\nDocument Title = " + documentTitle);
+
+            // Print out a list of wall types available in the current rvt project:
+
+            //WallTypeSet wallTypes = rvtDoc.WallTypes; // 2013, deprecated in 2014
+
+            FilteredElementCollector wallTypes // 2014
+              = new FilteredElementCollector(rvtDoc)
+                .OfClass(typeof(WallType));
+
+            string s = "";
+            foreach (WallType wallType in wallTypes)
+            {
+                s += wallType.Name + "\r\n";
+            }
+
+            // Show the result:
+
+            TaskDialog.Show(
+              "Revit Intro Lab",
+              "Wall Types (in main instruction):\n\n" + s);
+
+            // Return failure code to demonstrate output argument handling.
+            // 2nd and 3rd arguments are displayed to the user when the command fails.
+            // 2nd - set a message to display to the user.
+            // 3rd - set elements to highlight on screen.
+
+            return Result.Failed;
+        }
+
+        #region Revit Macros
+        public void MyFirstMacro()
+        {
+            TaskDialog.Show("My First Macro", "Hello World!");
+        }
+
+        public void MyFirstMacro2()
+        {
+            TaskDialog.Show("My First Macro",
+              "The current model file is ");
+            //+ this.Application.ActiveUIDocument.Document.PathName );
+        }
+        #endregion // Revit Macros
     }
-    #endregion // Revit Macros
-  }
 }
